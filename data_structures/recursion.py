e3ebbb4f7220262592a39d288d5d77ea75a11416
Jamie Barber <jamie.aba@gmail.com>
2023-08-16 19:40:06 -0500

    Recursion Day 2, Part 2

--- data_structures/recursion.py
+++ data_structures/recursion.py
@@ -14,18 +14,39 @@ def addString (array):
 
 '''Provides an array of even numbers only'''
 def FindEven(array, list_even=[]):
+    #Base Case - there's only one element and the function is to return a list
     if (len(array)== 1):
         if (array[0]%2 == 0):
             list_even.append(array[0])
             return list_even
         else:
             return list_even
+    #If not "Base Case", start with first element, add to the list, and call the function again to check the rest of the array
+    #Until we get to the "Base Case"
     elif (array[0]%2 == 0):
         list_even.append(array[0])
         return FindEven(array[1:],list_even)
     else:
         return FindEven(array[1:],list_even)
+    
+''' Triangular Numbers, returns a number that is the correct number from the series
+ e.g N number gives N + the previous number. If N = 4, then N[1] => 1, N[2] => N[1]+2 = 3, N[3] => N[2]+3 = 6, N[4] => N[3]+4 = 10 '''
+def TriangularNumber(N):
+    if N == 1:
+        return 1
+    else:
+        return N + TriangularNumber(N-1)
 
+'''Accepts a string and returns the index of the first x character found in the string'''
+def FindX(array, index = 0):
+    if (len(array) == 1):
+        return index
+    else:
+        if (array[0] == 'x'):
+            return index
+        else:
+            index+=1
+            return FindX(array[1:],index)
 
 
         
@@ -33,5 +54,7 @@ charString = ["helllo", "hi", "omg"]
 final = addString(charString)
 evenstr = [2, 7, 0, 5, 3, 8, 16]
 finale = FindEven(evenstr)
+trig = TriangularNumber(6)
+index = FindX("aaaaxaaax")
 print("hello")
-print(f'final count:{finale}')
+print(f'final count:{index}')
